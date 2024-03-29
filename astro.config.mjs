df02601ae0bd386dc63ba541e700b6b04e273987
Agustin Arenas <arenasagustin7@gmail.com>
2024-03-29 12:38:32 -0300

    Add Vercel adapter for web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig, squooshImageService } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   image: {
     service: squooshImageService(),
   },
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
