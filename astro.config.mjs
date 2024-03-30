72a09b896771dc4d9a0ada0ddd668e628fa42cb5
Agustin Arenas <arenasagustin7@gmail.com>
2024-03-30 16:51:58 -0300

    Update Astro configuration and package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,17 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   site: "https://agustinarenas.vercel.app",
   integrations: [tailwind(), sitemap()],
   image: {
-    service: squooshImageService(),
+    service: squooshImageService()
   },
   adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
